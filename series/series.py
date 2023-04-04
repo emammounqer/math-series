18a8e8f7ece01d9f229abb97d61f18415521ff40
Emam Mounqer <emam.anagreh@gmail.com>
2023-04-04 19:39:34 +0000

    complete fibonacci function and tests

--- series/series.py
+++ series/series.py
@@ -1,2 +1,4 @@
 def fibonacci(num):
-    return num
+    if num == 0 or num == 1:
+        return num
+    return fibonacci(num-1) + fibonacci(num-2)
--- tests/test_series.py
+++ tests/test_series.py
@@ -12,3 +12,21 @@ def test_one():
     actual = fibonacci(1)
     expected = 1
     assert actual == expected
+
+
+def test_two():
+    actual = fibonacci(2)
+    expected = 1
+    assert actual == expected
+
+
+def test_three():
+    actual = fibonacci(3)
+    expected = 2
+    assert actual == expected
+
+
+def test_six():
+    actual = fibonacci(6)
+    expected = 8
+    assert actual == expected
