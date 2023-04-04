5a77ca64187b8e169ec8c315697f375d50f1699d
Emam Mounqer <emam.anagreh@gmail.com>
2023-04-04 19:55:58 +0000

    add docstring to the fibonacci function

--- series/series.py
+++ series/series.py
@@ -1,4 +1,13 @@
-def fibonacci(num):
-    if num == 0 or num == 1:
-        return num
-    return fibonacci(num-1) + fibonacci(num-2)
+def fibonacci(n: int) -> int:
+    """
+    Return the nth number in the fibonacci sequence.
+
+    Args:
+        n (int): The index of the number in the sequence.
+
+    Returns:
+        int: The nth number in the fibonacci sequence.
+    """
+    if n == 0 or n == 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
